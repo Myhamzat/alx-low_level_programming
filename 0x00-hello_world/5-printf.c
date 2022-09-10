f7803246e924742f637efd01f792304705e613ad
Myhamzat <myhamzat@gmail.com>
2022-09-09 19:31:04 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - printthe string in the put function
+* Description: using the main fuction
+* this program prints "Programming is like building a multilingual puzzle"
+* Return: 0
+int main(void)
+{
+	printf("with grammer, but the outcome is a pieces of art,\n");
+	return (0);
+}
