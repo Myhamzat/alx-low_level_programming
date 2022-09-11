de6932205cf21e88d60333f0179a93534938606e
Myhamzat <myhamzat@gmail.com>
2022-09-11 14:52:17 -0700

    possibisble number combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,28 +9,29 @@
 
 int main(void)
 {
-    int i, p;
+    int ni = 0, n2;
+    while(n <=9)
 
-
-
-    for (i = 0; i < 100; i++)
     {
-        for (p = 0; p < 100; j++)
+        if (ni != n2 && ni n1 < n2)
         {
-            if (p > i)
-            {
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
+           
+                putchar((n1 / 10) + 48);
+                putchar((n1 % 10) + 48);
                 putchar(' ');
-                putchar((p / 10) + '0');
-                putchar((p % 10) + '0');
-                if (i != 98)
+		putchar((n2 / 10) + 48);
+		putchar((n2 % 10) +48);	
+
+                if (n1 != 98 || n2 != 99)
                 {
                     putchar(',');
                     putchar(' ');
                 }
-            }
-        }
+	}
+    ++n2;    
+    }
+   ++n1;
+
     }
 
 
