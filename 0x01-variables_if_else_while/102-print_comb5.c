83e25e45e5872c87658a84a008db703485bb6132
Myhamzat <myhamzat@gmail.com>
2022-09-11 15:31:43 -0700

    possible combination of 2 number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,20 +5,21 @@
  *
  * Return: returns zero at the end
  */
-
-
 int main(void)
 {
-    int ni = 0, n2;
-    while(n <=9)
-
-    {
-        if (ni != n2 && ni n1 < n2)
+ if (ni != n2 && ni n1 < n2)	
         {
-           
+            if (p > i)
+            {
+                putchar((i / 10) + '0');
+                putchar((i % 10) + '0');
+
                 putchar((n1 / 10) + 48);
                 putchar((n1 % 10) + 48);
                 putchar(' ');
+                putchar((p / 10) + '0');
+                putchar((p % 10) + '0');
+                if (i != 98)
 		putchar((n2 / 10) + 48);
 		putchar((n2 % 10) +48);	
 
@@ -27,16 +28,12 @@ int main(void)
                     putchar(',');
                     putchar(' ');
                 }
-	}
+}
     ++n2;    
     }
    ++n1;
 
     }
-
-
-    putchar('\n');
+putchar('\n');
     return (0);
 }
-
-
