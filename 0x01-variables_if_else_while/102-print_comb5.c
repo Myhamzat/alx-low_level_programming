711b837eb09932b5c8425cf6c33b944c2f756459
Myhamzat <myhamzat@gmail.com>
2022-09-12 17:00:05 -0700

    to print combination of 2 number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>                                                                                                                                                            
+/**                                                                                                                                                                           
+ * main - print possible combination of two 2-digit                                                                                                                           
+ * numbers                                                                                                                                                                    
+ *                                                                                                                                                                            
+ * Return: returns zero at the end                                                                                                                                            
+ */                                                                                                                                                                           
+int main()                                                                                                                                                                
+{                                                                                                                                                                             
+        int n1 = 0, n2;                                                                                                                                                       
+        while (n1 <= 9)                                                                                                                                                       
+        {                                                                                                                                                                     
+                                                                                                                                                                              
+        n2 = 0;                                                                                                                                                               
+        while(n2 <= 9)                                                                                                                                                                                                                                                                                                              
+	}
+       
+	if (n1 !=2 &&  n1 < n2)                                                                                                                                                                                                                                                                                                                          
+        {       
+	 if ( n1 != n2)                                                                                                                                                    
+            {                                                                                                                                                                 
+                                                                                                                                                                              
+                putchar((n1 / 10) + 48);                                                                                                                                      
+                putchar((n1 % 10) + 48);                                                                                                                                      
+                putchar( '' )                                                                                                                                                  
+                putchar((n2 / 10) + 48);                                                                                                                                      
+                putchar((n2 % 10) +48);                                                                                                                                       
+                                                                                                                                                                              
+                if (n1 != 98 || n2 != 98)                                                                                                                                     
+                {                                                                                                                                                             
+                    putchar(',');                                                                                                                                             
+                    putchar(' ');                                                                                                                                             
+                }                                                                                                                                                             
+                                                                                                                                                                              
+    ++n2;                                                                                                                                                                     
+    }                                                                                                                                                                         
+   ++n1;                                                                                                                                                                      
+                                                                                                                                                                              
+    }                                                                                                                                                                         
+putchar('\n');                                                                                                                                                                
+    return (0);                                                                                                                                                               
+}     
