b0da339c361b7578c99703ef10d57b317cd5886e
Myhamzat <myhamzat@gmail.com>
2022-09-19 04:00:58 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,14 @@
 
 /**
 *_strlen - returns the length of a string
-* @s:turn: returns length as integer;
-* Return: retur lenght as integer;
+* @s: string
+*Return: returns length as integer;
+*/
 
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++
-
-	return (len);
+int len = 0;
+while (*(s + len) != '\0')
+len++;
+return (len);
 }
