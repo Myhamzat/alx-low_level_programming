1b0913c6d486b1e2f73ce2588c1b316407049b05
Myhamzat <myhamzat@gmail.com>
2022-09-22 00:56:22 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int index = 0, src_len = 0;
+	int i;
+
+	for (i = 0; i < n && scr[i] != '\n'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	
-while (src[index++])
-src_len++;
-
-for (index = 0; src[index] && index < n; index++)
-dest[index] = src[index];
-
-for (index = src_len; index < n; index++)
-dest[index] = '\0';
-
-return (dest);
+	return (dest);
 }
